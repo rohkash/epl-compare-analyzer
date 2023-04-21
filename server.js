91a6da038eed87457c7fe20926e3ad9c0bbde32b
Rohan Kashiviswanathan <rohkash@gatech.edu>
2023-04-21 15:14:03 -0400

    test

--- server.js
+++ server.js
@@ -2,20 +2,21 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const app = express();
+constardf app = express();
 const port = process.env.PORT || 3000;
 
 const client = new MongoClient(process.env.MONGOCLIENT_URL, { useUnifiedTopology: true });
+
 async function start() {
   await client.connect();
   console.log('Connected to database');
 
   const db = client.db('test');
 
-  app.get('/standings', async (req, res) => {
-    const standings = await db.collection('standings').find().toArray();
-    res.json(standings);
+  app.get('/', (req, res) => {
+    res.send('Welcome to my app!');
   });
+  
   
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
